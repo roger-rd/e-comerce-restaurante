32d8ae4c9b57ead9e535b52c01cb6006bdec0335
roger rodriguez <rogerdavid.rd@gmail.com>
2023-07-27 17:39:28 -0400

    se hace una prueba moviendo la ruta

--- src/view/Perfil.jsx
+++ src/view/Perfil.jsx
@@ -48,7 +48,7 @@ export default function Perfil() {
 
   const getUsuarioData = async () => {
     const urlServer = "https://proyect-backend.onrender.com/api/v1/user";
-    const endpoint = "/perfil";
+    const endpoint = "/usuario";
     const token = localStorage.getItem("token");
 
     try {
