039ad0e5e4e7c15f8c9315a2f59f68e1e616731f
roger rodriguez <rogerdavid.rd@gmail.com>
2023-07-27 15:08:08 -0400

    se modifica la ruta de registro

--- src/view/Perfil.jsx
+++ src/view/Perfil.jsx
@@ -1,82 +1,80 @@
-import { useEffect, useContext } from "react";
-import { UserContext } from "../context/UserContext";
-import { perfilContext } from "../context/PerfilContext";
-import axios from "axios";
-
-export default function Perfil() {
-  const { setUsuario } = useContext(UserContext);
-  const { usuarios } = useContext(perfilContext);
-
-  const getUsuarioData = async (correo) => {
-    const urlServer = "https://proyect-backend.onrender.com/api/v1/user/usuario";
-    try {
-      const { data } = await axios.get(urlServer, {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        },
-        params: {
-          correo: correo,
-        },
-      });
-      setUsuario(data);
-    } catch (error) {
-      alert("Hubo un error al obtener los datos del usuario 🙁");
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    const usuarioLocalStorage = localStorage.getItem("usuario");
-    if (usuarioLocalStorage) {
-      const usuario = JSON.parse(usuarioLocalStorage);
-      getUsuarioData(usuario.correo);
-    }
-  }, []);
-
-
 // import { useEffect, useContext } from "react";
 // import { UserContext } from "../context/UserContext";
 // import { perfilContext } from "../context/PerfilContext";
-
 // import axios from "axios";
 
 // export default function Perfil() {
 //   const { setUsuario } = useContext(UserContext);
 //   const { usuarios } = useContext(perfilContext);
 
-
-
-//   const getUsuarioData = async () => {
+//   const getUsuarioData = async (correo) => {
 //     const urlServer = "https://proyect-backend.onrender.com/api/v1/user/usuario";
-//     const endpoint = ":id_usuario";
-//     const token = localStorage.getItem("token");
-
 //     try {
-//       const { data } = await axios.get(urlServer + endpoint, {
-//         headers: { Authorization: "Bearer " + token },
+//       const { data } = await axios.get(urlServer, {
+//         headers: {
+//           Authorization: "Bearer " + localStorage.getItem("token"),
+//         },
+//         params: {
+//           correo: correo,
+//         },
 //       });
-//       setUsuarioGlobal(data);
 //       setUsuario(data);
-//     } catch ({ response: { data: message } }) {
-//       alert(message + " 🙁");
-//       console.log(message);
+//     } catch (error) {
+//       alert("Hubo un error al obtener los datos del usuario 🙁");
+//       console.log(error);
 //     }
 //   };
 
 //   useEffect(() => {
-//     getUsuarioData();
-//   },[]);
+//     const usuarioLocalStorage = localStorage.getItem("usuario");
+//     if (usuarioLocalStorage) {
+//       const usuario = JSON.parse(usuarioLocalStorage);
+//       getUsuarioData(usuario.correo);
+//     }
+//   }, []);
+
+
+import { useEffect, useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import { perfilContext } from "../context/PerfilContext";
+
+import axios from "axios";
+
+export default function Perfil() {
+  const { setUsuario } = useContext(UserContext);
+  const { usuarios } = useContext(perfilContext);
 
 
 
-  // useEffect(() => {
-  //   const usuarioLocalStorage = localStorage.getItem("usuario");
-  //   if (usuarioLocalStorage) {
-  //     setUsuario(JSON.parse(usuarioLocalStorage));
-  //   }
-  // }, []);
+  const getUsuarioData = async () => {
+    const urlServer = "https://proyect-backend.onrender.com/api/v1/user";
+    const endpoint = "/perfil";
+    const token = localStorage.getItem("token");
+
+    try {
+      const { data } = await axios.get(urlServer + endpoint, {
+        headers: { Authorization: "Bearer " + token },
+      });
+      setUsuarioGlobal(data);
+      setUsuario(data);
+    } catch ({ response: { data: message } }) {
+      alert(message + " 🙁");
+      console.log(message);
+    }
+  };
+
+  useEffect(() => {
+    getUsuarioData();
+  },[]);
 
 
+
+  useEffect(() => {
+    const usuarioLocalStorage = localStorage.getItem("usuario");
+    if (usuarioLocalStorage) {
+      setUsuario(JSON.parse(usuarioLocalStorage));
+    }
+  }, []);
 
 
 
@@ -93,22 +91,21 @@ export default function Perfil() {
         <div className="row">
           <div>
             <div className="perfil">
-              {usuarios &&
-                usuarios.map((item) => (
-                  <div className="row-cols-1 row-cols-md-1 " key={item.id}>
+             
+                  <div className="row-cols-1 row-cols-md-1 ">
                     <div className="col">
                       <div className="card h-100">
                         <div className="card-body">
                           <h2>Datos Personales</h2>
-                          <h5 className="text-muted">Nombre y apellido: <span>{item.nombre} {item.apellido}</span></h5>
-                          <h5 className="text-muted">Rut: {item.rut}</h5>
-                          <h5 className="text-muted">Correo: {item.correo}</h5>
-                          <h5 className="text-muted">Teléfono: {item.telefono}</h5>
+                          <h5 className="text-muted">Nombre y apellido: <span>{usuarios.nombre} {usuarios.apellido}</span></h5>
+                          <h5 className="text-muted">Rut: {usuarios.rut}</h5>
+                          <h5 className="text-muted">Correo: {usuarios.correo}</h5>
+                          <h5 className="text-muted">Teléfono: {usuarios.telefono}</h5>
                         </div>
                         <hr />
                         <div className="card-footer">
                           <h2>Dirección de entrega</h2>
-                          <h5 className="text-muted">Dirección: {item.direccion} {item.numero_de_direccion}</h5>
+                          <h5 className="text-muted">Dirección: {usuarios.direccion} {usuarios.numero_de_direccion}</h5>
                           {/* <h5 className="text-muted">Comuna: {item.comuna}</h5> */}
                         </div>
 
@@ -135,7 +132,6 @@ export default function Perfil() {
                       </div>
                     </div>
                   </div>
-                ))}
             </div>
           </div>
         </div>
@@ -143,3 +139,66 @@ export default function Perfil() {
     </>
   );
 }
+
+
+//   return (
+//     <>
+//       <div>
+//         <h1>
+//           <span className="badge bg-secondary">Bienvenid@</span>
+//         </h1>
+//       </div>
+//       <div className="container text-center">
+//         <div className="row">
+//           <div>
+//             <div className="perfil">
+//               {usuarios &&
+//                 usuarios.map((item) => (
+//                   <div className="row-cols-1 row-cols-md-1 " key={item.id}>
+//                     <div className="col">
+//                       <div className="card h-100">
+//                         <div className="card-body">
+//                           <h2>Datos Personales</h2>
+//                           <h5 className="text-muted">Nombre y apellido: <span>{item.nombre} {item.apellido}</span></h5>
+//                           <h5 className="text-muted">Rut: {item.rut}</h5>
+//                           <h5 className="text-muted">Correo: {item.correo}</h5>
+//                           <h5 className="text-muted">Teléfono: {item.telefono}</h5>
+//                         </div>
+//                         <hr />
+//                         <div className="card-footer">
+//                           <h2>Dirección de entrega</h2>
+//                           <h5 className="text-muted">Dirección: {item.direccion} {item.numero_de_direccion}</h5>
+//                           {/* <h5 className="text-muted">Comuna: {item.comuna}</h5> */}
+//                         </div>
+
+//                         <div className="buttonHome">
+//                           <button
+//                             className="btn btn-info"
+//                             onClick={() => {
+//                               // Agrega la lógica para la acción de "Editar perfil"
+//                               console.log("Editar perfil");
+//                             }}
+//                           >
+//                             Editar Perfil
+//                           </button>
+//                           <button
+//                             className="btn btn-danger"
+//                             onClick={() => {
+//                               // Agrega la lógica para la acción de "Eliminar usuario"
+//                               console.log("Eliminar usuario");
+//                             }}
+//                           >
+//                             Eliminar
+//                           </button>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 ))}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
